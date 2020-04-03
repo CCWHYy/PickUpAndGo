e7ca16b0c3d7919fe30560dd309379495fab13ab
Wadoch <dwadoch@gmail.com>
2020-04-03 22:03:43 +0200

    Setup react app with router

---
+++ ClientApp/src/routes.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import { MainScreen } from './screens/Main';
+
+export const Routes = () => (
+    <Switch>
+        <Route path='/'>
+            <MainScreen />
+        </Route>
+    </Switch>
+);
---
+++ ClientApp/src/screens/Main/Main.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const MainScreen = () => (
+    <div>
+        Ci co wygrają hackyeah
+    </div>
+);
