b1fd529a104ba00da35df68c4088f162f43edf91
Wadoch <dwadoch@gmail.com>
2020-04-03 23:19:38 +0200

    Add basic redux integration

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from 'react-redux';
+import { configureStore } from "@reduxjs/toolkit";
 
 import { Routes } from "./routes";
+import rootReducer from './redux/reducers';
+
+const store = configureStore({ reducer: rootReducer });
 
 const App = () => (
     <div>
-        <BrowserRouter>
-            <Routes />
-        </BrowserRouter>
+        <Provider store={ store }>
+            <BrowserRouter>
+                <Routes />
+            </BrowserRouter>
+        </Provider>
     </div>
 );
 
