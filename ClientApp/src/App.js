2480dc3ed1f12ba579d17200b6981efc7ce4667e
Wadoch <dwadoch@gmail.com>
2020-04-03 23:40:17 +0200

    Add theme provider

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from '@material-ui/core/styles';
 import { Provider } from 'react-redux';
 import { configureStore } from "@reduxjs/toolkit";
 
 import { Routes } from "./routes";
 import rootReducer from './redux/reducers';
+import { theme } from "./theme";
 
 const store = configureStore({ reducer: rootReducer });
 
 const App = () => (
     <div>
         <Provider store={ store }>
-            <BrowserRouter>
-                <Routes />
-            </BrowserRouter>
+            <ThemeProvider theme={ theme }>
+                <BrowserRouter>
+                    <Routes />
+                </BrowserRouter>
+            </ThemeProvider>
         </Provider>
     </div>
 );
