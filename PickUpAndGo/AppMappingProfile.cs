4c429b374833c41757b7d1be4a3f7f971e37ac94
dominikwantuch <dominikwantuch@protonmail.com>
2020-04-04 04:54:07 +0200

    Add AutoMapper default mapping profile

---
+++ PickUpAndGo/AppMappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using PickUpAndGo.Models;
+using PickUpAndGo.Persistence.Entities;
+
+namespace PickUpAndGo
+{
+    public class AppMappingProfile : Profile
+    {
+        public AppMappingProfile()
+        {
+            CreateMap<CreateUserModel, User>();
+            CreateMap<User, UserModel>();
+        }
+    }
+}
