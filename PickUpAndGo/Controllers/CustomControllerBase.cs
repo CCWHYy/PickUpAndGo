02ad73edfe84d9d3d3729599e62ca0898b2bf5bb
dominikwantuch <dominikwantuch@protonmail.com>
2020-04-04 05:17:36 +0200

    Add customControllerBase

---
+++ PickUpAndGo/Controllers/CustomControllerBase.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PickUpAndGo.Persistence.Context;
+
+namespace PickUpAndGo.Controllers
+{
+    /// <summary>
+    /// Custom controller base for code reuse
+    /// </summary>
+    public class CustomControllerBase : ControllerBase
+    {
+        /// <summary>
+        /// Auto-Mapper 
+        /// </summary>
+        protected IMapper Mapper { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        protected AppDbContext DbContext { get; set; }
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        /// <param name="mapper"></param>
+        public CustomControllerBase(AppDbContext dbContext, IMapper mapper)
+        {
+            DbContext = dbContext;
+            Mapper = mapper;
+        }
+    }
+}
