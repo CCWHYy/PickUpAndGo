aafe50ac573e095a2bd4b64d1609c4da004ec1d7
Franciszek Madej <f.madej@protonmail.com>
2020-04-04 17:24:06 +0200

    Add theme and icons

--- ClientApp/src/theme.js
+++ ClientApp/src/theme.js
@@ -1,5 +1,35 @@
 import { createMuiTheme } from "@material-ui/core";
 
-export const theme = createMuiTheme({});
+export const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#efebce",
+      light: "#ffffff",
+      darkColor: "#bcb99d",
+      contrastText: "#000000"
+    },
+    secondary: {
+      main: "#bb8588",
+      light: "#eeb5b8",
+      dark: "#8a585b",
+      contrastText: "#000000"
+    }
+  },
+  typography: {
+    fontFamily: [
+      "'Inria Sans'",
+      "-apple-system",
+      "BlinkMacSystemFont",
+      '"Segoe UI"',
+      "Roboto",
+      '"Helvetica Neue"',
+      "Arial",
+      "sans-serif",
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"'
+    ].join(",")
+  }
+});
 
 export default theme;
