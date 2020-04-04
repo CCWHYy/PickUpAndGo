897959a670b5cf5db0366cc71d985bafd0e8d39a
dominikwantuch <dominikwantuch@protonmail.com>
2020-04-04 05:16:51 +0200

    Add swagger

--- PickUpAndGo/Startup.cs
+++ PickUpAndGo/Startup.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
 using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -6,6 +9,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using PickUpAndGo.Persistence.Context;
 
 namespace PickUpAndGo
@@ -22,14 +26,33 @@ namespace PickUpAndGo
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // Map appsettings.json to AppSettings class
             var config = Configuration.Get<AppSettings>();
             services.Configure<AppSettings>(Configuration);
+
+            services.AddSwaggerGen(x =>
+            {
+                x.SwaggerDoc("v1", new OpenApiInfo()
+                {
+                    Title = "PickUpAnGo API",
+                    Version = "v1",
+                    Contact = new OpenApiContact()
+                    {
+                        Name = "JuniorSeniors",
+                        Email = "some@email.here"
+                    }
+                });
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                x.IncludeXmlComments(xmlPath);
+            });
 
             // Configure Auto-mapper
             var mappingConfig = new MapperConfiguration(configure =>
             {
                 configure.AddProfile(new AppMappingProfile());
             });
+
             var mapper = mappingConfig.CreateMapper();
             services.AddSingleton(mapper);
 
@@ -42,6 +65,12 @@ namespace PickUpAndGo
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSwagger();
+            app.UseSwaggerUI(x =>
+            {
+                x.SwaggerEndpoint("/swagger/v1/swagger.json", "PickUpAndGo API v1");
+                x.RoutePrefix = string.Empty;
+            });
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
