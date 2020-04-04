b5db434d3c4a72587d883737b30a644bf473161d
dominikwantuch <dominikwantuch@protonmail.com>
2020-04-04 04:50:38 +0200

    Add appsettings template class

---
+++ PickUpAndGo/AppSettings.cs
@@ -0,0 +1,12 @@
+﻿namespace PickUpAndGo
+{
+    public class AppSettings
+    {
+        public ConnectionStrings ConnectionStrings { get; set; }
+    }
+
+    public class ConnectionStrings
+    {
+        public string Database { get; set; }
+    }
+}
