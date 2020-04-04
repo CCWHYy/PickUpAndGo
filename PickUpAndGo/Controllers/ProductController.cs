561bd433c99ce59a2b74bfb3520244f056836244
dominikwantuch <dominikwantuch@protonmail.com>
2020-04-04 18:20:53 +0200

    Add basic crud endpoints

--- PickUpAndGo/Controllers/ProductController.cs
+++ PickUpAndGo/Controllers/ProductController.cs
@@ -1,9 +1,137 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PickUpAndGo.Persistence.Context;
 
 namespace PickUpAndGo.Controllers
 {
-    public class ProductController: ControllerBase
+    /// <summary>
+    /// Product Controller
+    /// </summary>
+    [Route("api/products")]
+    public class ProductController : CustomControllerBase
     {
-        
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        /// <param name="dbContext"></param>
+        /// <param name="mapper"></param>
+        public ProductController(AppDbContext dbContext, IMapper mapper) : base(dbContext, mapper)
+        {
+        }
+
+        //TODO Change positive response types from object to appropriate model!
+
+
+        /// <summary>
+        /// Get by ID
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("{id}")]
+        [ProducesResponseType(typeof(object), 200)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(500)]
+        public IActionResult GetById([FromRoute, Required] string id)
+        {
+            try
+            {
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return InternalServerError();
+            }
+        }
+
+        /// <summary>
+        /// Get All
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [ProducesResponseType(typeof(object), 200)]
+        [ProducesResponseType(500)]
+        public IActionResult GetAll()
+        {
+            try
+            {
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return InternalServerError();
+            }
+        }
+
+        /// <summary>
+        /// Add product
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        [ProducesResponseType(typeof(object), 201)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(409)]
+        [ProducesResponseType(500)]
+        public IActionResult Add()
+        {
+            try
+            {
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return InternalServerError();
+            }
+        }
+
+        /// <summary>
+        /// Update product
+        /// </summary>
+        /// <returns></returns>
+        [HttpPut]
+        [ProducesResponseType(typeof(object), 200)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(500)]
+        public IActionResult Update()
+        {
+            try
+            {
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return InternalServerError();
+            }
+        }
+
+
+        /// <summary>
+        /// Delete product
+        /// </summary>
+        /// <returns></returns>
+        [HttpDelete("{id}")]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(500)]
+        public IActionResult Delete([FromRoute, Required] string id)
+        {
+            try
+            {
+                return NoContent();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return InternalServerError();
+            }
+        }
     }
 }
--- PickUpAndGo/Controllers/StoreController.cs
+++ PickUpAndGo/Controllers/StoreController.cs
@@ -1,8 +1,137 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PickUpAndGo.Persistence.Context;
 
 namespace PickUpAndGo.Controllers
 {
-    public class StoreController : ControllerBase
+    /// <summary>
+    /// Product Controller
+    /// </summary>
+    [Route("api/products")]
+    public class StoreController : CustomControllerBase
     {
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        /// <param name="dbContext"></param>
+        /// <param name="mapper"></param>
+        public StoreController(AppDbContext dbContext, IMapper mapper) : base(dbContext, mapper)
+        {
+        }
+
+        //TODO Change positive response types from object to appropriate model!
+
+
+        /// <summary>
+        /// Get by ID
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("{id}")]
+        [ProducesResponseType(typeof(object), 200)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(500)]
+        public IActionResult GetById([FromRoute, Required] string id)
+        {
+            try
+            {
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return InternalServerError();
+            }
+        }
+
+        /// <summary>
+        /// Get All
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [ProducesResponseType(typeof(object), 200)]
+        [ProducesResponseType(500)]
+        public IActionResult GetAll()
+        {
+            try
+            {
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return InternalServerError();
+            }
+        }
+
+        /// <summary>
+        /// Add new store
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        [ProducesResponseType(typeof(object), 201)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(409)]
+        [ProducesResponseType(500)]
+        public IActionResult Add()
+        {
+            try
+            {
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return InternalServerError();
+            }
+        }
+
+        /// <summary>
+        /// Update store
+        /// </summary>
+        /// <returns></returns>
+        [HttpPut]
+        [ProducesResponseType(typeof(object), 200)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(500)]
+        public IActionResult Update()
+        {
+            try
+            {
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return InternalServerError();
+            }
+        }
+
+
+        /// <summary>
+        /// Delete store
+        /// </summary>
+        /// <returns></returns>
+        [HttpDelete("{id}")]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(500)]
+        public IActionResult Delete([FromRoute, Required] string id)
+        {
+            try
+            {
+                return NoContent();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return InternalServerError();
+            }
+        }
     }
 }
