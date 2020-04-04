b9d1cf0fc3546d86793313e84c3e7b989cde82d2
Franciszek Madej <f.madej@protonmail.com>
2020-04-04 18:38:58 +0200

    Fix copy

--- ClientApp/src/components/Shop/Shop.js
+++ ClientApp/src/components/Shop/Shop.js
@@ -1,82 +1,78 @@
-import React from 'react';
-import { useHistory } from 'react-router-dom';
+import React from "react";
+import { useHistory } from "react-router-dom";
 import { useDispatch } from "react-redux";
 
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
-import CardMedia from '@material-ui/core/CardMedia';
-import CardContent from '@material-ui/core/CardContent';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-import { green } from '@material-ui/core/colors';
-import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardHeader from "@material-ui/core/CardHeader";
+import CardMedia from "@material-ui/core/CardMedia";
+import CardContent from "@material-ui/core/CardContent";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import { green } from "@material-ui/core/colors";
+import ArrowForwardIosIcon from "@material-ui/icons/ArrowForwardIos";
 
 import { setDetails } from "../../redux/store/actions";
 
-const useStyles = makeStyles((theme) => ({
-    root: {
-        margin: 8,
-        width: 284,
-    },
-    media: {
-        height: 0,
-        paddingTop: '56.25%', // 16:9
-    },
-    avatar: {
-        backgroundColor: green[800],
-    },
-    goToShop: {
-        width: '100%',
-        padding: '16px 0',
-    },
+const useStyles = makeStyles(theme => ({
+  root: {
+    margin: 8,
+    width: 284
+  },
+  media: {
+    height: 0,
+    paddingTop: "56.25%" // 16:9
+  },
+  avatar: {
+    backgroundColor: green[800]
+  },
+  goToShop: {
+    width: "100%",
+    padding: "16px 0"
+  }
 }));
 
-export const Shop = (props) => {
-    const { name, location, logoImg, description } = props;
+export const Shop = props => {
+  const { name, location, logoImg, description } = props;
 
-    const classes = useStyles();
-    const history = useHistory();
-    const dispatch = useDispatch();
+  const classes = useStyles();
+  const history = useHistory();
+  const dispatch = useDispatch();
 
-    const goToStore = () => {
-        dispatch(setDetails(props));
-        history.push('/shop');
-    };
+  const goToStore = () => {
+    dispatch(setDetails(props));
+    history.push("/shop");
+  };
 
-    return (
-        <Card className={classes.root}>
-            <CardHeader
-                avatar={
-                    <Avatar aria-label="recipe" className={classes.avatar}>
-                        Ż
-                    </Avatar>
-                }
-                title={ name }
-                subheader={ location }
-            />
-            <CardMedia
-                className={classes.media}
-                image={ logoImg }
-                title={ name }
-            />
-            <CardContent>
-                <Typography variant="body2" color="textSecondary" component="div">
-                    {description}
-                </Typography>
-            </CardContent>
-            <Button
-                className={classes.goToShop}
-                color="primary"
-                variant="contained"
-                onClick={ goToStore }
-            >
-                Idź do sklepu
-                <ArrowForwardIosIcon />
-            </Button>
-        </Card>
-    );
+  return (
+    <Card className={classes.root}>
+      <CardHeader
+        avatar={
+          <Avatar aria-label="recipe" className={classes.avatar}>
+            Ż
+          </Avatar>
+        }
+        title={name}
+        subheader={location}
+      />
+      <CardMedia className={classes.media} image={logoImg} title={name} />
+      <CardContent>
+        <Typography variant="body2" color="textSecondary" component="div">
+          {description}
+        </Typography>
+      </CardContent>
+      <Button
+        className={classes.goToShop}
+        color="primary"
+        variant="contained"
+        onClick={goToStore}
+      >
+        Przejdź do oferty
+        <ArrowForwardIosIcon />
+      </Button>
+    </Card>
+  );
 };
 
 export default Shop;
