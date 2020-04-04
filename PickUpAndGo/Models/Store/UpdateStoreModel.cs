8824be968f7c7d96449e4e4be2a6a07553608451
dominikwantuch <dominikwantuch@protonmail.com>
2020-04-04 20:43:53 +0200

    Add UpdateStoreModel

---
+++ PickUpAndGo/Models/Store/UpdateStoreModel.cs
@@ -0,0 +1,16 @@
+﻿namespace PickUpAndGo.Models.Store
+{
+    public class UpdateStoreModel
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string StreetNumber { get; set; }
+        public string Route { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string PostalCode { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+}
