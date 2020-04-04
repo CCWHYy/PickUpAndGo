39eb99811377932a56fd30d618b591440d9cc16a
dominikwantuch <dominikwantuch@protonmail.com>
2020-04-04 19:46:43 +0200

    Remove StoreChainId from Store

---
+++ PickUpAndGo/Models/Store/StoreModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace PickUpAndGo.Models.Store
+{
+    public class StoreModel
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string StreetNumber { get; set; }
+        public string Route { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string PostalCode { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+}
