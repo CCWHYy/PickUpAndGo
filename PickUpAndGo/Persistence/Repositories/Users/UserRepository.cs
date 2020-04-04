03e07cc38110c1c755396e9c6048c9a1de16bcc4
Patryk Nguyen <patryknng@interia.pl>
2020-04-04 12:51:30 +0200

    Add user repository

---
+++ PickUpAndGo/Persistence/Repositories/Users/UserRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PickUpAndGo.Persistence.Context;
+using PickUpAndGo.Persistence.Entities;
+
+namespace PickUpAndGo.Persistence.Repositories.Users
+{
+    public class UserRepository : Repository<User>, IUserRepository
+    {
+        public UserRepository(AppDbContext context) : base(context)
+        {
+        }   
+    }
+}
