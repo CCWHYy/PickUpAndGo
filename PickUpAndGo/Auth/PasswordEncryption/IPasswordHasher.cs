d45494172d7650a16a8d8d5e0bd73f93d9ac2d49
dominikwantuch <dominikwantuch@protonmail.com>
2020-04-04 18:59:09 +0200

    Add password hasher

---
+++ PickUpAndGo/Auth/PasswordEncryption/IPasswordHasher.cs
@@ -0,0 +1,9 @@
+﻿namespace PickUpAndGo.Auth.PasswordEncryption
+{
+    public interface IPasswordHasher
+    {
+        string Hash(string password);
+
+        (bool Verified, bool NeedsUpgrade) Check(string hash, string password);
+    }
+}
