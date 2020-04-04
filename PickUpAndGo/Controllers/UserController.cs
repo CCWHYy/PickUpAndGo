b72697562f95bf968572e17f167e072b08d59942
dominikwantuch <dominikwantuch@protonmail.com>
2020-04-04 05:36:47 +0200

    Code refactoring

--- PickUpAndGo/Controllers/UserController.cs
+++ PickUpAndGo/Controllers/UserController.cs
@@ -1,35 +1,94 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
 using System.Threading.Tasks;
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using PickUpAndGo.Models;
 using PickUpAndGo.Persistence.Context;
+using PickUpAndGo.Persistence.Entities;
 
 namespace PickUpAndGo.Controllers
 {
+    /// <summary>
+    /// 
+    /// </summary>
     [Route("api/users")]
-    public class UserController : ControllerBase
+    public class UserController : CustomControllerBase
     {
-        private readonly AppDbContext _dbContext;
-
-        public UserController(AppDbContext dbContext)
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="dbContext"></param>
+        /// <param name="mapper"></param>
+        public UserController(AppDbContext dbContext, IMapper mapper) : base(dbContext, mapper)
         {
-            _dbContext = dbContext;
         }
-        
+
+        /// <summary>
+        /// Get user by ID
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         [HttpGet("{id}")]
+        [ProducesResponseType(typeof(UserModel), 200)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(409)]
+        [ProducesResponseType(500)]
         public IActionResult GetById([FromRoute, Required] string id)
         {
-            if (string.IsNullOrWhiteSpace(id))
-                return BadRequest("ID is required!");
-            
-            var user = _dbContext.Users.Find(id);
-            return Ok(user);
+            try
+            {
+                Console.WriteLine(id);
+                if (string.IsNullOrWhiteSpace(id))
+                    return BadRequest("ID is required!");
+
+                var user = DbContext.Users.Find(id);
+
+                if (user != null)
+                {
+                    var res = Mapper.Map<UserModel>(user);
+                    return Ok(res);
+                }
+
+                return NotFound("User with given ID does not exist!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return InternalServerError();
+            }
         }
 
-        
+        /// <summary>
+        /// Create user
+        /// </summary>
+        /// <param name="createUserModel"></param>
+        [HttpPost]
+        [ProducesResponseType(typeof(UserModel), 201)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(409)]
+        [ProducesResponseType(500)]
         public IActionResult Create([FromBody] CreateUserModel createUserModel)
         {
-            return Ok();
+            try
+            {
+                // TODO some validation before
+                var entity = Mapper.Map<User>(createUserModel);
+
+                var createResponse = DbContext.Users.Add(entity);
+
+                DbContext.SaveChanges();
+
+                var res = Mapper.Map<UserModel>(createResponse.Entity);
+
+                return Created(res);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return InternalServerError();
+            }
         }
     }
 }
