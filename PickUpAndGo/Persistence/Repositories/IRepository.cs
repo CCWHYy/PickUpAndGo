70e4638a0b4de2f28b2e9b786829d8064ab7fdcc
Patryk Nguyen <patryknng@interia.pl>
2020-04-04 12:47:11 +0200

    Add generic repositories

---
+++ PickUpAndGo/Persistence/Repositories/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace PickUpAndGo.Persistence.Repositories
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        TEntity Get(string id);
+        ICollection<TEntity> Find(Expression<Func<TEntity, bool>> predicate);
+        ICollection<TEntity> GetAll();
+        void Add(TEntity entity);
+        void Remove(TEntity entity);
+    }
+}
---
+++ PickUpAndGo/Persistence/Repositories/Repository.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace PickUpAndGo.Persistence.Repositories
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        protected readonly DbContext Context;
+
+        public Repository(DbContext context)
+        {
+            Context = context;
+        }
+
+        public void Add(TEntity entity)
+        {
+            Context.Set<TEntity>().Add(entity);
+        }
+
+        public ICollection<TEntity> GetAll()
+        {
+            return Context.Set<TEntity>().ToList();
+        }
+
+        public ICollection<TEntity> Find(Expression<Func<TEntity, bool>> predicate)
+        {
+            return Context.Set<TEntity>().Where(predicate).ToList();
+        }
+
+        public TEntity Get(string id)
+        {
+            return Context.Set<TEntity>().Find(id);
+        }
+
+        public void Remove(TEntity entity)
+        {
+            Context.Set<TEntity>().Remove(entity);
+        }
+    }
+}
