09a3fec6192ff8f8557624a19b3f97a335dc5d25
dominikwantuch <dominikwantuch@protonmail.com>
2020-04-04 04:53:41 +0200

    Add basic User DTO's

---
+++ PickUpAndGo/Models/UpdateUserModel.cs
@@ -0,0 +1,9 @@
+﻿namespace PickUpAndGo.Models
+{
+    public class UpdateUserModel
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
