317449afe10251b5d3695c7078954e07293afbc4
Patryk Nguyen <patryknng@interia.pl>
2020-04-04 19:47:17 +0200

    Add product dtos

---
+++ PickUpAndGo/Models/Product/CreateProductModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PickUpAndGo.Models.Product
+{
+    public class CreateProductModel
+    {
+        public string StoreId { get; set; }
+        public string Name { get; set; }
+        public string Brand { get; set; }
+        public double Quantity { get; set; }
+        public string QuantityUnit { get; set; }
+        public double Price { get; set; }
+        public string Category { get; set; }
+    }
+}
---
+++ PickUpAndGo/Models/Product/ProductModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PickUpAndGo.Models.Product
+{
+    public class ProductModel
+    {
+        public string Id { get; set; }
+        public string StoreId { get; set; }
+        public string Name { get; set; }
+        public string Brand { get; set; }
+        public double Quantity { get; set; }
+        public string QuantityUnit { get; set; }
+        public double Price { get; set; }
+        public string Category { get; set; }
+    }
+}
