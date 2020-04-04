862bf3ea9d7906566aeb6ff3ae1f58312e09c774
dominikwantuch <dominikwantuch@protonmail.com>
2020-04-04 04:50:27 +0200

    Add AppDbContext

---
+++ PickUpAndGo/Persistence/Context/AppDbContext.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PickUpAndGo.Persistence.Context.Configurations;
+using PickUpAndGo.Persistence.Entities;
+
+namespace PickUpAndGo.Persistence.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Store> Stores { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<StoreOrder> StoreOrders { get; set; }
+        public DbSet<OrderProduct> OrderProducts { get; set; }
+        public DbSet<EmployeeStore> EmployeeStores { get; set; }
+
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new UserConfiguration());
+            modelBuilder.ApplyConfiguration(new StoreConfiguration());
+            modelBuilder.ApplyConfiguration(new ProductConfiguration());
+            modelBuilder.ApplyConfiguration(new ProductConfiguration());
+            modelBuilder.ApplyConfiguration(new OrderConfiguration());
+            modelBuilder.ApplyConfiguration(new EmployeeConfiguration());
+            modelBuilder.ApplyConfiguration(new StoreOrderConfiguration());
+            modelBuilder.ApplyConfiguration(new OrderProductConfiguration());
+            modelBuilder.ApplyConfiguration(new EmployeeStoreConfiguration());
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
