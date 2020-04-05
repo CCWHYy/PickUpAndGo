36d3989b9aa8ddca6ac0da126876d2bafcb0f3d3
dominikwantuch <dominikwantuch@protonmail.com>
2020-04-05 04:39:16 +0200

    Add logo url to Store

--- PickUpAndGo/Models/Store/CreateStoreModel.cs
+++ PickUpAndGo/Models/Store/CreateStoreModel.cs
@@ -11,5 +11,6 @@
         public string PostalCode { get; set; }
         public double Latitude { get; set; }
         public double Longitude { get; set; }
+        public string LogoUrl { get; set; }
     }
 }
